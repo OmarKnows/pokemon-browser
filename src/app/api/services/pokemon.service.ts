fdc7c34d82d55eb9b9de43d5512546edd066deb1
OmarKnows <omarknows98@gmail.com>
2025-11-14 15:46:54 +0200

    created pokemon service and types

---
+++ src/app/api/services/pokemon.service.ts
@@ -0,0 +1,22 @@
+import { ENDPOINTS } from '../../constants/server';
+import instance from '../axios.config';
+import type { IListResponse } from '../types/api.types';
+import type { IPokemon, IPokemonListItem } from '../types/pokemon.types';
+
+class PokemonService {
+	public async list(params?: any): Promise<IListResponse<IPokemonListItem>> {
+		const { data } = await instance.get(ENDPOINTS.POKEMONS, {
+			params,
+		});
+
+		return data;
+	}
+
+	public async getById(id: number): Promise<IPokemon> {
+		const { data } = await instance.get(`${ENDPOINTS.POKEMONS}/${id}`);
+
+		return data;
+	}
+}
+
+export default Object.freeze(new PokemonService());
---
+++ src/app/api/types/pokemon.types.ts
@@ -0,0 +1,82 @@
+import type { INamedAPIResource } from './api.types';
+
+export interface IPokemon {
+	id: number;
+	name: string;
+	base_experience: number;
+	height: number;
+	weight: number;
+	is_default: boolean;
+	order: number;
+
+	abilities: PokemonAbility[];
+	forms: INamedAPIResource[];
+	game_indices: VersionGameIndex[];
+	held_items: PokemonHeldItem[];
+	location_area_encounters: string;
+
+	moves: PokemonMove[];
+	sprites: PokemonSprites;
+	species: INamedAPIResource;
+
+	stats: PokemonStat[];
+	types: PokemonType[];
+}
+
+export interface IPokemonListItem extends INamedAPIResource {}
+
+interface PokemonAbility {
+	is_hidden: boolean;
+	slot: number;
+	ability: INamedAPIResource;
+}
+
+interface VersionGameIndex {
+	game_index: number;
+	version: INamedAPIResource;
+}
+
+interface PokemonHeldItemVersion {
+	version: INamedAPIResource;
+	rarity: number;
+}
+
+interface PokemonHeldItem {
+	item: INamedAPIResource;
+	version_details: PokemonHeldItemVersion[];
+}
+
+interface PokemonMoveVersion {
+	move_learn_method: INamedAPIResource;
+	version_group: INamedAPIResource;
+	level_learned_at: number;
+}
+
+interface PokemonMove {
+	move: INamedAPIResource;
+	version_group_details: PokemonMoveVersion[];
+}
+
+interface PokemonSprites {
+	front_default: string | null;
+	front_shiny: string | null;
+	front_female: string | null;
+	front_shiny_female: string | null;
+	back_default: string | null;
+	back_shiny: string | null;
+	back_female: string | null;
+	back_shiny_female: string | null;
+	other?: any;
+	versions?: any;
+}
+
+interface PokemonStat {
+	base_stat: number;
+	effort: number;
+	stat: INamedAPIResource;
+}
+
+interface PokemonType {
+	slot: number;
+	type: INamedAPIResource;
+}
