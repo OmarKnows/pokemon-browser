722da88556df0aae0f6f538948b778d1f49e94e5
OmarKnows <omarknows98@gmail.com>
2025-11-14 15:54:51 +0200

    setup tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			refetchOnReconnect: false,
+			refetchOnMount: false,
+			staleTime: 1000 * 60 * 5,
+			gcTime: 1000 * 60 * 30,
+			retry: false,
+		},
+	},
+});
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<QueryClientProvider client={queryClient}>
+		<App />
+	</QueryClientProvider>
+);
