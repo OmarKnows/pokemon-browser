0ad4573e71389c2e2fff2bbc8169ced8aab05d95
OmarKnows <omarknows98@gmail.com>
2025-11-15 02:17:33 +0200

    updated constants

---
+++ src/app/constants/localstorage.ts
@@ -0,0 +1,7 @@
+export const STORAGE_KEY = {
+	VIEW_MODE: 'view-mode',
+	ITEMS_PER_PAGE: 'items-per-page',
+};
+
+export const DEFAULT_ITEMS_PER_PAGE = 20;
+export const ITEMS_PER_PAGE_OPTIONS = [10, 20, 50, 100];
