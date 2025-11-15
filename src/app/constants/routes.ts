95cf2faec3efcbf04359aa11a1207ef044f7e313
OmarKnows <omarknows98@gmail.com>
2025-11-15 02:09:58 +0200

    adjusted pokemon routes heirarchy

--- src/app/constants/routes.ts
+++ src/app/constants/routes.ts
@@ -1,3 +1,3 @@
 export const ROUTES = {
-	POKEMON: 'pokemon',
+	POKEMON: '/pokemon',
 };
--- src/app/router/routes/pokemon.routes.tsx
+++ src/app/router/routes/pokemon.routes.tsx
@@ -1,15 +1,22 @@
 import type { RouteObject } from 'react-router';
 import { ROUTES } from '../../constants/routes';
-import PokemonList from '../../../modules/pokemon/pages/PokemonList';
 import PokemonDetails from '../../../modules/pokemon/pages/PokemonDetails';
+import PokemonList from '../../../modules/pokemon/pages/PokemonList';
+import Error from '../Error';
 
 export const pokemonRoutes: RouteObject[] = [
 	{
 		path: ROUTES.POKEMON,
-		element: <PokemonList />,
-	},
-	{
-		path: `${ROUTES.POKEMON}/:id`,
-		element: <PokemonDetails />,
+		errorElement: <Error />,
+		children: [
+			{
+				index: true,
+				element: <PokemonList />,
+			},
+			{
+				path: ':id',
+				element: <PokemonDetails />,
+			},
+		],
 	},
 ];
